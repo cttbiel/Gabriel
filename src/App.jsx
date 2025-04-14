6ad0a0e37f1544af09810582e802178323683b09
Gabriel Carvalho <cttbiel@gmail.com>
2025-04-14 11:33:57 -0300

    Footer added
    Need to work on responsive, mobile it's fine, but desktop not that much

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import Carvalho from './sections/Carvalho/Carvalho';
+import Contact from './sections/Contact/Contact';
+import Footer from './sections/Footer/Footer';
 import Projects from './sections/Projects/Projects';
 import Skills from './sections/Skills/Skills';
 
@@ -10,9 +12,14 @@ function App() {
       <Carvalho />
       <Projects />
       <Skills />
+      <Contact />
+      <Footer />
     </>
   );
   
 }
 
 export default App;
+
+
+/*1:48:56*/
